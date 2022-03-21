f03ead84084331f672e8ac10c722a9f6074f9b1f
NamgyungKim <namgyung.kim@gmail.com>
2022-03-21 17:41:59 +0900

    ✨ feat: GlobalStyle setting

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -4,8 +4,19 @@ import reset from 'styled-reset';
 const GlobalStyles = createGlobalStyle`
     //style reset
     ${reset}
+    
+    body {
+        color: var(--text-color);
+        background-color: #E5E5E5;
+    }
 
     :root {
+        --main-color: #FF8450;
+        --text: #5B5555;
+        --light-text: #7D7878;
+        --dark-gray: #B6B3B3;
+        --gray: #E2E2E2;
+        --light-gray: #EEEEEE;
     }
 `;
 
