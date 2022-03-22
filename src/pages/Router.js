ed2766d456923771bc3061abf092739b4de289b4
NamgyungKim <namgyung.kim@gmail.com>
2022-03-23 01:34:49 +0900

    🚚 move: BrowserRouter 위치 변경

--- src/pages/Router.js
+++ src/pages/Router.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Address from './Address';
 import Completion from './Completion';
 import Confirm from './Confirm';
@@ -9,16 +9,14 @@ import Type from './Type';
 
 const Router = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/type" element={<Type />} />
-        <Route path="/schedule" element={<Schedule />} />
-        <Route path="/address" element={<Address />} />
-        <Route path="/confirm" element={<Confirm />} />
-        <Route path="/completion" element={<Completion />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Main />} />
+      <Route path="/type" element={<Type />} />
+      <Route path="/schedule" element={<Schedule />} />
+      <Route path="/address" element={<Address />} />
+      <Route path="/confirm" element={<Confirm />} />
+      <Route path="/completion" element={<Completion />} />
+    </Routes>
   );
 };
 
